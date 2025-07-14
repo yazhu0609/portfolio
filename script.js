5c1b298e279e16a53f4d25ab3f7b712e8f8aa9ba
yazhu0609 <7010hope@gmail.com>
2025-07-14 11:06:21 +0800

    Modify contact section.

---
+++ script.js
@@ -0,0 +1,16 @@
+function changeToAbout(){
+    let aboutDiv=document.querySelector("#aboutcon");
+    let expsDiv=document.querySelector("#exps");
+
+    aboutDiv.style.display="block";
+    expsDiv.style.display="none";
+}
+
+
+function changeToExps(){
+    let aboutDiv=document.querySelector("#aboutcon");
+    let expsDiv=document.querySelector("#exps");
+
+    aboutDiv.style.display="none";
+    expsDiv.style.display="block";
+}
