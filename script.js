5ec3d4bbe2fb283ea25169adf8ba1b1a57eb3475
yazhu0609 <7010hope@gmail.com>
2025-07-15 11:41:25 +0800

    Modify RWD and add new project4、project 5.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//點擊文字切換內文
 function changeToAbout(){
     let aboutDiv=document.querySelector("#aboutcon");
     let expsDiv=document.querySelector("#exps");
@@ -5,7 +6,6 @@ function changeToAbout(){
     aboutDiv.style.display="block";
     expsDiv.style.display="none";
 }
-
 
 function changeToExps(){
     let aboutDiv=document.querySelector("#aboutcon");
@@ -15,7 +15,7 @@ function changeToExps(){
     expsDiv.style.display="block";
 }
 
-// 輪播圖片邏輯
+//輪播圖片
 let currentIndex = 0;
 const images = document.querySelectorAll(".carousel-img");
 const prevBtn = document.querySelector(".carousel-btn.prev");
@@ -27,6 +27,7 @@ function showImage(index) {
     });
 }
 
+//輪播圖片按鈕點擊
 prevBtn.addEventListener("click", () => {
     currentIndex = (currentIndex - 1 + images.length) % images.length;
     showImage(currentIndex);
@@ -37,9 +38,39 @@ nextBtn.addEventListener("click", () => {
     showImage(currentIndex);
 });
 
-// 自動輪播（每5秒切換）
+//自動輪播（每5秒切換）
 setInterval(() => {
     currentIndex = (currentIndex + 1) % images.length;
     showImage(currentIndex);
 }, 5000);
+
+const btn = document.querySelector(".menu-btn");
+const nav = document.querySelector("nav");
+const topBtn = document.querySelector("#top");
+
+//顯示漢堡選單
+if (btn) {
+    btn.addEventListener("click", () => {
+        btn.classList.toggle("clicked");
+    });
+}
+//漢堡選單點擊後關閉
+if (nav) {
+    nav.addEventListener("click", (e) => {
+        if (e.target.tagName === "A") {
+            btn.classList.remove("clicked");
+        }
+    });
+}
+//回頂點後，top按鈕隱藏
+if (topBtn) {
+    document.addEventListener("scroll", () => {
+        if (window.scrollY > 0) {
+            topBtn.classList.remove("hide");
+        } else {
+            topBtn.classList.add("hide");
+        }
+    });
+}
+
 
